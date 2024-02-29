dc9ecde9e2eb399be2fc7bcc6ce59f7c68d89f51
Alfredo Romero <Alfredo_Romero@jec.senate.gov>
2024-02-29 15:29:13 -0500

    Dropping JECTools

--- R/convertToJson.R
+++ R/convertToJson.R
@@ -1,15 +1,44 @@
 library(dplyr)
 library(tidyr)
 library(jsonlite)
-library(JECTools)
+library(fredr)
 
 
-convert_to_json_format <- function(csv_file_path) {
-  states_unemployment <- get_unemployment(geography = 'State', 
-                                          start_year = 2021,
-                                          fred_key = FRED_API_KEY,
-                                          BLS_key = BLS_API_KEY) |>
-    transform_to_tibble()
+fredr::fredr_set_key(FRED_API_KEY)
+
+states <- data.frame(state = c(state.abb,"DC"))
+
+start_year <- 2020
+
+end_year <- 2024
+
+variables <- NULL
+for (state in states$state) {
+  tmp <- paste0(state, "UR")
+  variables <- c(variables, tmp)
+}
+
+variables <- c(variables, "UNRATE")
+
+num_series <- length(variables)
+
+# Paparameters of the request
+params <- list(
+    series_id = variables,
+    observation_start = rep(as.Date(paste0(start_year,"-01","-01")),
+                            num_series),
+    observation_end = rep(as.Date(paste0(end_year,"-12","-31")), num_series))
+  
+  # Actual request of data
+states_unemployment <- purrr::pmap_dfr(
+    .l = params,
+    .f = ~ fredr::fredr(series_id = ..1,
+                        observation_start = ..2, observation_end = ..3)) %>%
+    dplyr::select(-c(realtime_start, realtime_end)) |>
+    tidyr::pivot_wider(names_from = series_id, values_from = value) |>
+    dplyr::rename(USUR = UNRATE) |> 
+    dplyr::as_tibble()
+  
   states_names <- c("date",state.name,"District of Columbia","National")
   names(states_unemployment) <- states_names
   
@@ -27,4 +56,4 @@ convert_to_json_format <- function(csv_file_path) {
   json_data <- toJSON(json_list, auto_unbox = TRUE, pretty = TRUE)
   
   write(json_data, file="unemploymentActions.JSON")
-}
+  
